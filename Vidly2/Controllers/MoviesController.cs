edca37805300f65391482a2b3c99b8f66ed63321
PKraakman <pkraakman@shindigz.com>
2019-01-08 17:04:24 -0500

    movies edit added

--- Vidly2/Controllers/MoviesController.cs
+++ Vidly2/Controllers/MoviesController.cs
@@ -23,8 +23,6 @@ namespace Vidly2.Controllers
             base.Dispose(disposing);
         }
 
-    
-
         // GET: Movies
         public ActionResult Random()
         {
@@ -42,28 +40,9 @@ namespace Vidly2.Controllers
             };
 
             return View(viewModel);
-            // return Content("hello world");
-            // return HttpNotFound();
-            // return new EmptyResult();
-            // return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
         }
 
-        // movies/edit
-
-        public ActionResult Edit(int id)
-        {
-            //return Content(String.Format("Now editing id: {0}", id));
-
-            //foreach (var movie  in movies)
-            //{
-            //    if (movie.Id == id)
-            //        return View(movie);
-            //}
-
-            return View(new Movie());
-        }
-
-
+   
         // movies/Index
         public ActionResult Index(int? pageIndex, string sortby)
         {
@@ -81,7 +60,22 @@ namespace Vidly2.Controllers
             return View(viewModel);
         }
 
-        //movies/details
+
+        // movies/new
+        public ActionResult New()
+        {
+
+            var viewModel = new MoviesFormViewModel
+            {
+                Movie = new Movie(),
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+
+        // movies/details
         public ActionResult Details(int Id)
         {
             var movie = _context.Movies.Include(m => m.Genre).FirstOrDefault(m => m.Id == Id);
@@ -90,6 +84,43 @@ namespace Vidly2.Controllers
                 return HttpNotFound();
 
             return View(movie);
+        }
+
+
+        // movies/edit
+        public ActionResult Edit(int Id)
+        {
+            var movie = _context.Movies.Include(m => m.Genre).FirstOrDefault(m => m.Id == Id);
+            var viewModel = new MoviesFormViewModel()
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        // movies/save
+        public ActionResult Save(Movie Movie)
+        {
+            if (Movie.Id == 0)
+            {
+                Movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(Movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == Movie.Id);
+
+                movieInDb.Name = Movie.Name;
+                movieInDb.ReleaseDate = Movie.ReleaseDate;
+                movieInDb.GenreId = Movie.GenreId;
+                movieInDb.QtyInStock = Movie.QtyInStock;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
         }
 
         // movies/released/{year}/{month}
--- Vidly2/Models/Movie.cs
+++ Vidly2/Models/Movie.cs
@@ -2,17 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly2.Models
 {
     public class Movie      
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Genre")]
         public int GenreId { get; set; }
+
         public Genre Genre { get; set; }
+
+        [Display(Name = "Release Date")]
+        [Required]
         public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Date Added")]
+        [Required]
         public DateTime DateAdded { get; set; }
+
+        [Required]
         public int QtyInStock { get; set; }
     }
 }
